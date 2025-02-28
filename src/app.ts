e29a3bc329fc6793c3f0192c3ab7c9d4f7d48ea0
priyankambliya <priyankambliya18@gmail.com>
2025-02-28 23:19:31 +0530

    Add welcome route to the main application

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => { res.send('Welcome to the app') })
 app.use('/api', appRoutes)
 
 app.use(errorHandler.unHandledRouteHandler)
